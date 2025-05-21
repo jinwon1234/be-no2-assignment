87cda86ae26e12e1239dea8129bb9b0255122b94
jinwon <ljw0626@naver.com>
2025-05-21 17:37:56 +0900

    first commit

---
+++ src/main/java/kakao/beno2homework/Beno2homeworkApplication.java
@@ -0,0 +1,13 @@
+package kakao.beno2homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beno2homeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beno2homeworkApplication.class, args);
+	}
+
+}
