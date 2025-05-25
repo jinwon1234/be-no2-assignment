526676e1e17d07e8da5700f03e7a7abfeb0d3b20
jinwon <ljw0626@naver.com>
2025-05-25 16:15:17 +0900

    sql 파일 수정

---
+++ schedule.sql
@@ -0,0 +1,32 @@
+CREATE database v1;
+CREATE database v2;
+
+
+USE v1;
+CREATE TABLE schedule (
+        schedule_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+        author VARCHAR(255) NOT NULL ,
+        content VARCHAR(255) NOT NULL,
+        create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        password VARCHAR(255) NOT NULL
+);
+
+USE v2;
+CREATE TABLE member (
+        member_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+        update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE schedule (
+        schedule_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+        member_id BIGINT NOT NULL,
+        content TEXT NOT NULL,
+        create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+        update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        FOREIGN KEY (member_id) REFERENCES member(member_id) ON DELETE CASCADE
+);
